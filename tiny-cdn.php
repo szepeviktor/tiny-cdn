6a7daff845b274868674fd55eed02ec3554cf405
Viktor Szépe <viktor@szepe.net>
2017-04-10 12:45:54 +0000

    Some comments and improvement

--- tiny-cdn.php
+++ tiny-cdn.php
@@ -7,8 +7,8 @@ Author: Viktor Szépe
 License: GNU General Public License (GPL) version 2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/szepeviktor/tiny-cdn
-Options: TINY_CDN_CONTENT_URL
-Options: TINY_CDN_INCLUDES_URL
+Constants: TINY_CDN_CONTENT_URL
+Constants: TINY_CDN_INCLUDES_URL
 */
 
 final class O1_Tiny_Cdn {
@@ -25,9 +25,14 @@ final class O1_Tiny_Cdn {
         add_action( 'template_redirect', array( $this, 'hooks' ) );
     }
 
+    /**
+     * Rewrite only URL-s WordPress core "knows about".
+     */
     public function hooks() {
 
-        if ( apply_filters( 'tiny_cdn_disable', false ) ) {
+        $capability = apply_filters( 'tiny_cdn_capability', 'edit_pages' );
+
+        if ( apply_filters( 'tiny_cdn_disable', false ) || ( current_user_can( $capability ) ) {
             return;
         }
 
@@ -37,20 +42,23 @@ final class O1_Tiny_Cdn {
         // ? add_filter( 'includes_url', array( $this, 'rewrite_includes' ), 9999 );
         // ? add_filter( 'content_url', array( $this, 'rewrite_content' ), 9999 );
 
-        // Rewrite styles and scripts
+        // Rewrite style and script URL-s
         add_filter( 'script_loader_src', array( $this, 'rewrite' ), 9999 );
         add_filter( 'style_loader_src', array( $this, 'rewrite' ), 9999 );
 
-        // Rewrite /wp-content files
+        // Rewrite /wp-content file URL-s
         add_filter( 'plugins_url', array( $this, 'rewrite_content' ), 9999 );
         add_filter( 'theme_root_uri', array( $this, 'rewrite_content' ), 9999 );
         // ? wp_get_attachment_image_src
         add_filter( 'upload_dir', array( $this, 'uploads' ), 9999 );
 
-        // Rewrite post_content
+        // Rewrite URL-s in post_content
         add_filter( 'the_content', array( $this, 'images' ), 9999 );
     }
 
+    /**
+     * Rewrite both includes and content URL-s.
+     */
     public function rewrite( $url ) {
 
         if ( 1 === preg_match( $this->excludes, $url ) ) {
@@ -63,6 +71,9 @@ final class O1_Tiny_Cdn {
         return $url;
     }
 
+    /**
+     * Rewrite content URL.
+     */
     public function rewrite_content( $url ) {
 
         if ( 1 === preg_match( $this->excludes, $url ) ) {
@@ -74,6 +85,9 @@ final class O1_Tiny_Cdn {
         return $url;
     }
 
+    /**
+     * Replace includes URL if the given constant is present.
+     */
     private function replace_includes( $url ) {
 
         if ( ! defined( 'TINY_CDN_INCLUDES_URL' ) ) {
@@ -86,6 +100,9 @@ final class O1_Tiny_Cdn {
         return $url;
     }
 
+    /**
+     * Replace content URL if the given constant is present.
+     */
     private function replace_content( $url ) {
 
         if ( ! defined( 'TINY_CDN_CONTENT_URL' ) ) {
@@ -97,6 +114,9 @@ final class O1_Tiny_Cdn {
         return $url;
     }
 
+    /**
+     * Rewrite uploads URL.
+     */
     public function uploads( $upload_data ) {
 
         $upload_data['url'] = $this->rewrite_content( $upload_data['url'] );
@@ -105,11 +125,14 @@ final class O1_Tiny_Cdn {
         return $upload_data;
     }
 
+    /**
+     * Rewrite image URL-s in post content.
+     */
     public function images( $content ) {
 
         // Only catch images inserted with the editor
-        //           (              1              )(  2  )(      3      )
-        $pattern = '|(<img [^>]*class="[^"]+" src=")([^"]+)(" alt="[^"]*")|';
+        //           (        1        )(  2  )(      3      )
+        $pattern = '|(<img [^>]*\bsrc=")([^"]+)(" [^>]*\balt="[^"]*")|';
 
         $content = preg_replace_callback(
             $pattern,
@@ -127,18 +150,21 @@ final class O1_Tiny_Cdn {
 new O1_Tiny_Cdn();
 
 /*
-Rewrite only URL-s WordPress core "knows about"
 Works with custom wp-content location
 No multisite support -> wp-cdn-rewrite get_rewrite_path()
 define( 'TINY_CDN_INCLUDES_URL', 'https://d2aaaaaaaaaaae.cloudfront.net/wp-includes' );
 define( 'TINY_CDN_CONTENT_URL', 'https://d2aaaaaaaaaaae.cloudfront.net/wp-content' );
+filter example: at 'init'
+tiny_cdn_disable
+tiny_cdn_capability
+tiny_cdn_excludes
 
  https://www.itsupportguides.com/knowledge-base/wordpress/wordpress-how-to-serve-static-content-from-a-cookieless-domain/
  https://codex.wordpress.org/Editing_wp-config.php#Set_Cookie_Domain
  define( 'COOKIE_DOMAIN', site_url()/domain part );
  Test cookies on CDN!
 
- Don't use CDN when user has capability filter(X)
+ Don't use CDN when user has capability
  Avoid query string -> resource-versioning
 
  W3TC
