a070e46179938ac1a406beda2198320b98b1312b
Viktor Szépe <viktor@szepe.net>
2018-10-14 22:27:28 +0000

    Hook only init and template_redirect

--- tiny-cdn.php
+++ tiny-cdn.php
@@ -9,82 +9,107 @@
  * GitHub Plugin URI: https://github.com/szepeviktor/tiny-cdn
  * Constants: TINY_CDN_INCLUDES_URL
  * Constants: TINY_CDN_CONTENT_URL
+ *
+ * @package tiny-cdn
  */
 
+/**
+ * Tiny CDN plugin in the form of a class.
+ */
 final class O1_Tiny_Cdn {
 
     /**
-     * Exclusion pattern
+     * Exclusion pattern.
      *
      * @var string $excludes
      */
     private $excludes;
 
+    /**
+     * Bootstrap plugin.
+     */
     public function __construct() {
 
         if ( is_admin() || ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ) {
             return;
         }
 
-        // Filter to disable TinyCDN early
-        if ( true === apply_filters( 'tiny_cdn_disable_early', false ) ) {
-            return;
-        }
+        // Add early rewrites.
+        add_action( 'init', array( $this, 'init' ) );
 
-        // Yoast sitemap is generated outside the template
-        add_filter( 'wpseo_xml_sitemap_img_src', array( $this, 'rewrite_content' ), 9999 );
-        // Expose rewrite_content early as a filter for rewrites before the template
-        add_filter( 'tiny_cdn_early', array( $this, 'rewrite_content' ) );
-        // Add rewrites for template pages
+        // Add rewrites for template pages.
         add_action( 'template_redirect', array( $this, 'hooks' ) );
     }
 
     /**
-     * Rewrite only URL-s WordPress core "knows about".
+     * Add hooks for filters before the template.
+     */
+    public function init() {
+
+        // Filter to disable early hooks.
+        if ( true === apply_filters( 'tiny_cdn_disable_early', false ) ) {
+            return;
+        }
+
+        // Early excludes regexp.
+        $this->excludes = apply_filters( 'tiny_cdn_excludes_early', '#\.php#' );
+
+        // Yoast sitemap is generated in pre_get_posts hook.
+        add_filter( 'wpseo_xml_sitemap_img_src', array( $this, 'rewrite_content' ), 9999 );
+
+        // Expose rewrite_content early as a filter for rewrites.
+        add_filter( 'tiny_cdn_early', array( $this, 'rewrite_content' ) );
+    }
+
+    /**
+     * Hook only URL-s WordPress core "knows about".
      */
     public function hooks() {
 
-        // Capability for rewriting frontend asset URL-s
+        // Capability for rewriting frontend asset URL-s.
         $capability = apply_filters( 'tiny_cdn_capability', 'edit_pages' );
-        // Filter to disable TinyCDN from the template
+        // Filter to disable TinyCDN from the template.
         if ( apply_filters( 'tiny_cdn_disable', false ) || current_user_can( $capability ) ) {
             return;
         }
 
-        // Excludes regexp
+        // Excludes regexp.
         $this->excludes = apply_filters( 'tiny_cdn_excludes', '#\.php#' );
 
-        // Don't go this deep
-        //add_filter( 'includes_url', array( $this, 'rewrite_includes' ), 9999 );
-        //add_filter( 'content_url', array( $this, 'rewrite_content' ), 9999 );
+        /**
+         * Don't go this deep.
+         *
+         * add_filter( 'includes_url', array( $this, 'rewrite_includes' ), 9999 );
+         * add_filter( 'content_url', array( $this, 'rewrite_content' ), 9999 );
+         */
 
-        // Rewrite URL-s of files under /wp-content
+        // Rewrite URL-s of files under /wp-content.
         add_filter( 'plugins_url', array( $this, 'rewrite_content' ), 9999 );
         add_filter( 'theme_root_uri', array( $this, 'rewrite_content' ), 9999 );
         add_filter( 'upload_dir', array( $this, 'uploads' ), 9999 );
 
-        // Rewrite style and script URL-s
+        // Rewrite style and script URL-s.
         add_filter( 'script_loader_src', array( $this, 'rewrite' ), 9999 );
         add_filter( 'style_loader_src', array( $this, 'rewrite' ), 9999 );
 
-        // Rewrite URL-s in post_content
+        // Rewrite URL-s in post_content.
         add_filter( 'the_content', array( $this, 'images' ), 9999 );
         add_filter( 'widget_text', array( $this, 'images' ), 9999 );
 
-        // Post thumbnail
+        // Post thumbnail.
         add_filter( 'wp_get_attachment_image_src', array( $this, 'thumbnail' ), 9999 );
 
-        // Third-parties
+        // Third-parties.
         add_filter( 'wpseo_opengraph_image', array( $this, 'rewrite_content' ), 9999 );
 
-        // Expose rewrite_content as a filter, e.g. for Resource Versioning plugin
+        // Expose rewrite_content as a filter, e.g. for Resource Versioning plugin.
         add_filter( 'tiny_cdn', array( $this, 'rewrite_content' ) );
     }
 
     /**
      * Rewrite both includes URL and content URL.
      *
-     * @param string $url
+     * @param string $url Any URL.
      * @return string
      */
     public function rewrite( $url ) {
@@ -102,7 +127,7 @@ final class O1_Tiny_Cdn {
     /**
      * Rewrite content URL.
      *
-     * @param string $url
+     * @param string $url Any URL.
      * @return string
      */
     public function rewrite_content( $url ) {
@@ -119,7 +144,7 @@ final class O1_Tiny_Cdn {
     /**
      * Replace includes URL if the given constant is present.
      *
-     * @param string $url
+     * @param string $url Any URL.
      * @return string
      */
     private function replace_includes( $url ) {
@@ -137,7 +162,7 @@ final class O1_Tiny_Cdn {
     /**
      * Replace content URL if the given constant is present.
      *
-     * @param string $url
+     * @param string $url Any URL.
      * @return string
      */
     private function replace_content( $url ) {
@@ -154,7 +179,7 @@ final class O1_Tiny_Cdn {
     /**
      * Rewrite uploads URL.
      *
-     * @param array $upload_data
+     * @param array $upload_data Upload data.
      * @return array
      */
     public function uploads( $upload_data ) {
@@ -168,13 +193,15 @@ final class O1_Tiny_Cdn {
     /**
      * Rewrite image URL-s in post content.
      *
-     * @param string $content
+     * @param string $content Post content.
      * @return string
      */
     public function images( $content ) {
 
-        // Only catch images inserted with the editor
-        //           (        1        )(  2  )(         3          )
+        /**
+         * Only catch images inserted with the editor
+         *           (        1        )(  2  )(         3          )
+         */
         $pattern = '|(<img [^>]*\bsrc=")([^"]+)(" [^>]*\balt="[^"]*")|';
 
         $content = preg_replace_callback(
@@ -192,7 +219,7 @@ final class O1_Tiny_Cdn {
     /**
      * Rewrite image URL of post thumbnail.
      *
-     * @param array|false $image
+     * @param array|false $image Image data.
      * @return array|false
      */
     public function thumbnail( $image ) {
@@ -205,4 +232,5 @@ final class O1_Tiny_Cdn {
     }
 }
 
+// Start!
 new O1_Tiny_Cdn();
